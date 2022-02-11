f5c830ec5ae35141ee54cd3b6fb4c84e15ecf57b
bermudezj <bermudezjbq@gmail.com>
2022-02-11 16:42:52 +0100

    tester

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
-import Header from './components/Header'
-import Main from './components/Main'
-import Footer from './components/Footer'
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+import "./styles/styles.scss";
 
 const App = () => {
   return (
     <div className="App">
-      <Header/>
-      <Main/>
-      <Footer/>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
--- client/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="footer">
-      <h2>Footer</h2>
-    </footer>
-  );
-};
-
-export default Footer;
